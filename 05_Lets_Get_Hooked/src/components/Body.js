4e3814094096eadce1d0bf5e99c9cdfe09438848
abhagarg <abha.garg287@gmail.com>
2024-05-28 19:01:49 +0530

    adding useState hook

--- 05_Lets_Get_Hooked/src/components/Body.js
+++ 05_Lets_Get_Hooked/src/components/Body.js
@@ -1,11 +1,22 @@
 import { RestaurantCard } from "./RestaurantCard";
 import { resObj } from "../utils/mockData";
+import { useState } from "react";
 const Body = () => {
+    const [listOfRes, setListOfRes] = useState(resObj);
     return (
         <div className="body">
-            <div className="search">Search bar aaega idhar (meow!)</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const topResList = listOfRes.filter((res) =>
+                        res.info.avgRating >= 4);
+                    setListOfRes(topResList);
+                }}>
+                    Top Restaurants in town!
+                </button>
+            </div>
+            
             <div className="card-container">
-                {resObj.map((restaurant) => (
+                {listOfRes.map((restaurant) => (
                     <RestaurantCard key = {restaurant.info.id} resData = {restaurant} />
                 ))}
             </div>
--- 05_Lets_Get_Hooked/src/components/RestaurantCard.js
+++ 05_Lets_Get_Hooked/src/components/RestaurantCard.js
@@ -1,14 +1,14 @@
 import { CDN_URL1 } from "../utils/constants";
 export const RestaurantCard = ({resData}) => {
 
-    const { name, cuisines, costForTwo,cloudinaryImageId} = resData?.info;
-    const { avgRating, deliveryTime } = resData?.info?.sla;
+    const { name, cuisines, costForTwo,cloudinaryImageId, avgRatingString } = resData?.info;
+    const { deliveryTime } = resData?.info?.sla;
     return (
         <div className="card">
                     <img src= {CDN_URL1 + cloudinaryImageId} ></img>
                     <h3>{name}</h3>
                     <h4>{cuisines.join(', ')}</h4>
-                    <h4>{avgRating}</h4>
+                    <h4>{avgRatingString}</h4>
                     <h4>{deliveryTime} minutes</h4>
                     <h4>{costForTwo}/2</h4>
         </div>
